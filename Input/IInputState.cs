d553c9b58bfd1dcf5f6a5fb75b554f2e2845aee0
Zacklatta <zalatta@ksu.edu>
2026-09-23 18:51:47 -0500

    Finished Tutorial

---
+++ Input/IInputState.cs
@@ -0,0 +1,18 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace CIS580.Input;
+
+public interface IInputState
+{
+    public bool IsKeyPressed(Keys key);
+
+    bool IsNewKeyPress(Keys key);
+
+
+    bool IsButtonPressed(Buttons button, PlayerIndex? controllingPlayer, out PlayerIndex playerIndex);
+
+
+    bool IsNewButtonPress(Buttons button, PlayerIndex? controllingPlayer, out PlayerIndex playerIndex);
+
+}
